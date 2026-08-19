284c91d94f65a61afa76c2205ea5412c4a0a9e29
Ishan <chhaparwalishan@gmail.com>
2026-08-19 15:28:53 +0530

    Time: 0 ms (100%), Space: 9.4 MB (42.54%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> freq;
+
+        for (char ch : s) {
+            freq[ch]++;
+        }
+
+        int ans = 0;
+        bool odd = false;
+
+        for (auto& it : freq) {
+            if (it.second % 2 == 0)
+                ans += it.second;
+            else {
+                ans += it.second - 1;
+                odd = true;
+            }
+        }
+
+        if (odd)
+            ans++;
+
+        return ans;
+    }
+};
